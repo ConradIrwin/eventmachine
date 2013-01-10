3ce39bc44a1cebbdb053fd145df99fde3a778b78
Conrad Irwin <conrad.irwin@gmail.com>
2013-01-09 20:48:50 -0800

    Add EM::Connection#get_server_name_indication

--- ext/ed.h
+++ ext/ed.h
@@ -73,6 +73,7 @@ class EventableDescriptor: public Bindable_t
 
 		#ifdef WITH_SSL
 		virtual X509 *GetPeerCert() {return NULL;}
+		virtual const char *GetServerNameIndication(){return NULL;}
 		#endif
 
 		virtual uint64_t GetCommInactivityTimeout() {return 0;}
@@ -199,6 +200,7 @@ class ConnectionDescriptor: public EventableDescriptor
 		virtual X509 *GetPeerCert();
 		virtual bool VerifySslPeer(const char*);
 		virtual void AcceptSslPeer();
+		virtual const char *GetServerNameIndication();
 		#endif
 
 		void SetServerMode() {bIsServer = true;}
@@ -237,6 +239,7 @@ class ConnectionDescriptor: public EventableDescriptor
 		SslBox_t *SslBox;
 		std::string CertChainFilename;
 		std::string PrivateKeyFilename;
+		std::string ServerNameIndication;
 		bool bHandshakeSignaled;
 		bool bSslVerifyPeer;
 		bool bSslPeerAccepted;
